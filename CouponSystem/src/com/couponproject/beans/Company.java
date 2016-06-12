d292e01d7e340971ef1816d531243f6c7f031c63
Avi <Avi@Avi-Lp>
2016-06-12 16:23:46 +0300

    company bean fixed

--- CouponSystem/src/com/couponproject/beans/Company.java
+++ CouponSystem/src/com/couponproject/beans/Company.java
@@ -1,5 +1,57 @@
 package com.couponproject.beans;
 
 public class Company {
+	private long id;
+	private String compName;
+	private String password;
+	private String email;
 
+	
+	public Company(long id, String compName, String password, String email) {
+		super();
+		this.id = id;
+		this.compName = compName;
+		this.password = password;
+		this.email = email;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public String getCompName() {
+		return compName;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public void setCompName(String compName) {
+		this.compName = compName;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
