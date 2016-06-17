7a30676907fd493d302710e274bc30fbe9fc433d
Orit Blum <oritsa@gmail.com>
2016-06-17 21:26:02 +0300

    creating couponDBDAO, adding throw exceptions to couponDAO

--- CouponSystem/src/com/couponproject/dao/CouponDAO.java
+++ CouponSystem/src/com/couponproject/dao/CouponDAO.java
@@ -1,5 +1,6 @@
 package com.couponproject.dao;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 import com.couponproject.beans.*;
@@ -9,21 +10,21 @@ public interface CouponDAO {
 	//TODO: Exception??
 	
 	//this method gets instance of Coupon and adds it in to the Coupons table in the DB
-	public void createCoupon(Coupon coupon);
+	public void createCoupon(Coupon coupon) throws SQLException;
 
 	//this method gets instance of Coupon and removes it from the Coupons table in the DB
-	public void removeCoupon(Coupon coupon);
+	public void removeCoupon(Coupon coupon) throws SQLException;
 
 	//this method gets instance of Coupon and updates it in the Coupons table in the DB
-	public void updateCoupon(Coupon coupon);
+	public void updateCoupon(Coupon coupon) throws SQLException;
 	
 	//this method gets Coupon's ID and returns instance of Coupon that this ID belongs to
-	public Coupon getCoupon(long id);
+	public Coupon getCoupon(long id) throws SQLException;
 	
 	//this method returns a Collection of all the Coupons in the couopon table in the DB
-	public Collection<Coupon> getAllCoupons();
+	public Collection<Coupon> getAllCoupons() throws SQLException;
 	
 	//this method gets a couponType and returns a Collection of Coupons that have that couponType
-	public Collection<Coupon> getCouponsByType(CouponType couponType);
+	public Collection<Coupon> getCouponsByType(CouponType couponType) throws SQLException;
 	
 }
