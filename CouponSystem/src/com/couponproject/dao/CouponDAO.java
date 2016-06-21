3d008e72e990f3e564465f6a76cec3dd414879c4
Orit Blum <oritsa@gmail.com>
2016-06-21 20:55:08 +0300

    DAO dona

--- CouponSystem/src/com/couponproject/dao/CouponDAO.java
+++ CouponSystem/src/com/couponproject/dao/CouponDAO.java
@@ -6,26 +6,29 @@ import java.util.Collection;
 import com.couponproject.beans.*;
 import com.couponproject.exception.CouponSystemException;
 
+//this interface specify's all the functions 
+//of a Coupon in relation to the data base
 public interface CouponDAO {
+	// ************
+	// Methods list
+	// ************
 	
-	//TODO: Exception??
-	
-	//this method gets instance of Coupon and adds it in to the Coupons table in the DB
-	public void createCoupon(Coupon coupon) throws SQLException, CouponSystemException;
+	//This method gets instance of Coupon and adds it in to the Coupons table in the DB
+	public void createCoupon(Coupon coupon) throws CouponSystemException;
 
-	//this method gets instance of Coupon and removes it from the Coupons table in the DB
-	public void removeCoupon(Coupon coupon) throws SQLException, CouponSystemException;
+	//This method gets instance of Coupon and removes it from the Coupons table in the DB
+	public void removeCoupon(Coupon coupon) throws CouponSystemException;
 
-	//this method gets instance of Coupon and updates it in the Coupons table in the DB
-	public void updateCoupon(Coupon coupon) throws SQLException, CouponSystemException;
+	//This method gets instance of Coupon and updates it in the Coupons table in the DB
+	public void updateCoupon(Coupon coupon) throws CouponSystemException;
 	
-	//this method gets Coupon's ID and returns instance of Coupon that this ID belongs to
-	public Coupon getCoupon(long id) throws SQLException, CouponSystemException;
+	//This method gets Coupon's ID and returns instance of Coupon that this ID belongs to
+	public Coupon getCoupon(long id) throws CouponSystemException;
 	
-	//this method returns a Collection of all the Coupons in the couopon table in the DB
-	public Collection<Coupon> getAllCoupons() throws SQLException, CouponSystemException;
+	//This method returns a Collection of all the Coupons in the coupon table in the DB
+	public Collection<Coupon> getAllCoupons() throws CouponSystemException;
 	
-	//this method gets a couponType and returns a Collection of Coupons that have that couponType
-	public Collection<Coupon> getCouponsByType(CouponType couponType) throws SQLException, CouponSystemException;
+	//This method gets a couponType and returns a Collection of Coupons that have that couponType
+	public Collection<Coupon> getCouponsByType(CouponType couponType) throws CouponSystemException;
 	
 }
