f5c6ce9491ced2849f1c2d0f38f612346356eac7
Avi <Avi@Avi-Lp>
2016-07-02 14:44:59 +0300

    test login

--- CouponSystem/src/com/couponproject/facade/CustomerFacadeTest.java
+++ CouponSystem/src/com/couponproject/facade/CustomerFacadeTest.java
@@ -3,14 +3,18 @@ package com.couponproject.facade;
 import com.couponproject.exception.FacadeException;
 
 public class CustomerFacadeTest {
+	
+	// CouponClientFacade instance for testing
 	private static CouponClientFacade facade;
 	
 	public static void main(String[] args) {
 		
 		try {
-			facade = CustomerFacade.login("customer 100", "password 100");
 			
+			// test login
+			facade = CustomerFacade.login("customer 100", "password 100");
 			System.out.println(facade);
+ 
 			
 		} catch (FacadeException e) {
 			e.toString();
