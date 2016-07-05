a7dbc2664008f3016fdcb35ddd9df9f2cf56a74f
Avi <Avi@Avi-Lp>
2016-07-05 23:45:28 +0300

    DailyCouponExportationTask - Thread

---
+++ CouponSystem/src/com/couponproject/exception/DailyCouponExportationTaskException.java
@@ -0,0 +1,25 @@
+package com.couponproject.exception;
+
+public class DailyCouponExportationTaskException extends CouponSystemException {
+
+	public DailyCouponExportationTaskException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DailyCouponExportationTaskException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DailyCouponExportationTaskException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DailyCouponExportationTaskException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ CouponSystem/src/com/couponproject/threads/DailyCouponExportationTask.java
@@ -0,0 +1,49 @@
+package com.couponproject.threads;
+
+import java.time.LocalDate;
+
+import com.couponproject.beans.Coupon;
+import com.couponproject.dbdao.CouponDBDAO;
+import com.couponproject.exception.CouponSystemException;
+
+/*
+ *  This is the DailyCouponExportationTask thread
+ *  it's runs every 24 hours and deletes all expired 
+ *  coupons from the DB/
+ */
+
+public class DailyCouponExportationTask implements Runnable {
+	// Access to DB throw CouponDBDAO
+	private CouponDBDAO coupDbdao = new CouponDBDAO();
+	
+	// Quit boolean 
+	private boolean quit = false;
+
+	@Override
+	public void run() {
+		while (!quit) {
+			try {
+				// get all coupons from DB
+				for (Coupon coupon : coupDbdao.getAllCoupons()) {
+
+					// check if coupon end date as past
+					if (LocalDate.now().isAfter(coupon.getEndDate())) {
+						coupDbdao.removeCoupon(coupon);
+					}
+				}
+				// Sleep for 24 Hours
+				Thread.sleep(1000 * 60 * 60 * 24);
+			} catch (CouponSystemException | InterruptedException e) {
+				// TODO what souled happen here??!!?!?!
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void stopTask(){
+		quit = true;
+	}
+	
+	
+	
+}
