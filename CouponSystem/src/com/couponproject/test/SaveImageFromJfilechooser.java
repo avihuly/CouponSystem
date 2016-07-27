86ac49cdbbeea81f89f3e2e728cd4010e3930b0b
Avi <Avi@10.30.0.174>
2016-07-27 20:01:12 +0300

    SaveImageFromJfilechooser - Test

--- CouponSystem/src/com/couponproject/test/SaveImageFromJfilechooser.java
+++ CouponSystem/src/com/couponproject/test/SaveImageFromJfilechooser.java
@@ -1,11 +1,19 @@
 package com.couponproject.test;
 
+import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-
+import javax.swing.JLabel;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class SaveImageFromJfilechooser extends JFrame {
@@ -33,8 +41,29 @@ public class SaveImageFromJfilechooser extends JFrame {
 			saveImageFromJfilechooser.dispose();
 			break;
 		case JFileChooser.APPROVE_OPTION:
-			ImageIO.write(new , "png", fileChooser.getSelectedFile());
+			String imagePath = fileChooser.getSelectedFile().getAbsolutePath();
+			String imageName = fileChooser.getSelectedFile().getName();
+			FileInputStream in;
+			try {
+				in = new FileInputStream(imagePath);
 
+				String targetPath = "image/CouponPics/" + imageName;
+				System.out.println(targetPath);
+				FileOutputStream out;
+
+				out = new FileOutputStream(targetPath);
+
+				int bytesRead = 0;
+				byte[] bucket = new byte[256];
+
+				// Use the bucket to move information from src to dests
+				while ((bytesRead = in.read(bucket)) > -1) {
+					out.write(bucket, 0, bytesRead);
+				}
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
 			break;
 		}
 	}
