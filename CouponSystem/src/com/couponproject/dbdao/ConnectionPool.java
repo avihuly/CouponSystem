4842a45ec02e5602071295521ccdc9011acf0152
Avi <Avi@Avi-Lp>
2016-09-22 23:05:33 +0300

    New connection pool

--- CouponSystem/src/com/couponproject/dbdao/ConnectionPool.java
+++ CouponSystem/src/com/couponproject/dbdao/ConnectionPool.java
@@ -5,32 +5,18 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-
 //External connection pool
 public class ConnectionPool {
 	private static final String URL = "jdbc:mysql://localhost:3306/coupondb";
 	private static final String USER_NAME = "root";
 	private static final String PASSWORD = "zaq1zaq1zaq1";
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
 	
 	private static ConnectionPool connectionPool;
-	private ComboPooledDataSource cpds;
+	private AssafConnectionPool acp;
 
 	public ConnectionPool() throws IOException, SQLException, PropertyVetoException {
-		cpds = new ComboPooledDataSource();
-		cpds.setDriverClass("com.mysql.jdbc.Driver"); // loads the jdbc driver
-		// Path
-		cpds.setJdbcUrl(URL);
-		// User name
-		cpds.setUser(USER_NAME);
-		// Password
-		cpds.setPassword(PASSWORD);
-
-		// the settings below are optional -- c3p0 can work with defaults
-		cpds.setMinPoolSize(5);
-		cpds.setAcquireIncrement(5);
-		cpds.setMaxPoolSize(10);
-		cpds.setMaxStatements(180);
+		acp = new AssafConnectionPool(DRIVER, URL, USER_NAME, PASSWORD, 5, 10, true);
 	}
 
 	public static ConnectionPool getInstance() throws IOException, SQLException, PropertyVetoException {
@@ -41,11 +27,11 @@ public class ConnectionPool {
 	}
 
 	public Connection getConnection() throws SQLException {
-		return this.cpds.getConnection();
+		return this.acp.getConnection();
 	}
 	
 	public void shutDown () {
-		cpds.close();
+		acp.closeAllConnections();
 	}
 
 }
