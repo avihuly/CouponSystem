d672f09bb4bc058bc628e9c4df5e533982fc972e
Avi <Avi@Avi-Lp.home>
2016-11-16 02:31:32 +0200

    !

--- CouponSystem/src/com/couponproject/dbdao/CouponDBDAO.java
+++ CouponSystem/src/com/couponproject/dbdao/CouponDBDAO.java
@@ -26,7 +26,6 @@ public class CouponDBDAO implements CouponDAO{
 	// Takes Coupon as argument and adds it to the coupon table in the DB
 	@Override
 	public void createCoupon(Coupon coupon) throws CouponSystemException, CouponTitleAlreadyExistException {
-		System.out.println(coupon);
 		if (Util.isCoupon(coupon)) {
 			throw new CouponTitleAlreadyExistException(Constants.CouponTitleAlreadyMassage);
 		} else {
